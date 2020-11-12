97ad6c6de716c7a87b694f4b6d894c0b8b6924b2
Personal-IliyanDimov <ilianmd@abv.bg>
2020-11-13 00:53:12 +0200

    Initial setup ...

---
+++ src/main/java/org/imd/jpa/sample1/JpaSample1Application.java
@@ -0,0 +1,13 @@
+package org.imd.jpa.sample1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaSample1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaSample1Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/imd/jpa/sample1/JpaSample1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.imd.jpa.sample1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaSample1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
